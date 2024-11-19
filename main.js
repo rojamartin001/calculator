f71299d636cc664ac98bbbbac47dcade0b83d5b3
Roja Martin <rojamartin678@gmail.com>
2024-11-19 11:28:24 +0530

    msg

---
+++ main.js
@@ -0,0 +1,16 @@
+// function to display content
+function displayContent(content){
+    result.value +=content
+}
+// clear input
+function calcClear(){
+    result.value = ""
+}
+// result
+function calcOutput(){
+    result.value= eval(result.value)
+}
+// backspace
+function removeLastDigit(){
+    result.value = result.value.slice(0,-1)
+}
